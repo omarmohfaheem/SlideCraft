5afaac1b4e44fb6823236040145f75b5e8eee864
Omar Mohamed Faheem <85244203+omarmohfaheem@users.noreply.github.com>
2024-06-01 02:12:45 +0300

    Some Changes
    Changed google chrome tab icon
    updated index and app with react-router-dom
    installed libraries

--- user-interface/frontend/src/App.js
+++ user-interface/frontend/src/App.js
@@ -1,17 +1,30 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Summarize from "./components/Summarize";
-import Help from "./components/Help";
+import Home from "./pages/Home";
+import CreatePresentation from "./pages/CreatePresentation";
+import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadApp = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      setIsLoading(false);
+    };
+    loadApp();
+  }, []);
+
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/summarize" element={<Summarize />}></Route>
-        <Route path="/help" element={<Help />}></Route>
-      </Routes>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/generator" element={<CreatePresentation />} />
+        </Routes>
+      )}
     </div>
   );
 }
