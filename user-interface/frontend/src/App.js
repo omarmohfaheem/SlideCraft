249a96050f47f6e32a1e2fb8b532b8a8a853d5d3
Omar Mohamed Faheem <85244203+omarmohfaheem@users.noreply.github.com>
2024-06-04 17:51:15 +0300

    Updated App.js
    New Routes
    Installed libraries

--- user-interface/frontend/src/App.js
+++ user-interface/frontend/src/App.js
@@ -1,28 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
-import CreatePresentation from "./pages/CreatePresentation";
+import SlideCreationPage from "./pages/SlideCreationPage";
+import Help from "./pages/Help";
 import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [initialLoad, setInitialLoad] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const location = useLocation();
+
+  const simulateLoading = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
 
   useEffect(() => {
     const loadApp = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-      setIsLoading(false);
+      await simulateLoading(3000);
+      setInitialLoad(false);
     };
     loadApp();
   }, []);
 
+  useEffect(() => {
+    const handleRouteChange = async () => {
+      setIsLoading(true);
+      await simulateLoading(1000);
+      setIsLoading(false);
+    };
+
+    handleRouteChange();
+  }, [location.pathname]);
+
   return (
     <div className="App">
-      {isLoading ? (
+      {initialLoad || isLoading ? (
         <LoadingScreen />
       ) : (
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/generator" element={<CreatePresentation />} />
+          <Route path="/slides-creator" element={<SlideCreationPage />} />
+          <Route path="/help" element={<Help />} />
         </Routes>
       )}
     </div>
